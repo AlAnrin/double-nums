377a5b2809f5fffbb112e57c8952ee78680c30c1
AlAnrin <almia.anrin@gmail.com>
2019-05-06 09:09:30 +0300

    build

---
+++ build/precache-manifest.0f3490af98d344ec22f4585b9206b22e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577ebc5e8b926c066a55a4599a7628fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a1d687bd26b06495043",
+    "url": "/static/css/main.65b19fd6.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7a1d687bd26b06495043",
+    "url": "/static/js/main.b7b424db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
