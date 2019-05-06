fc31d0a87f08bcbe84e3f5911b971993baa5bd46
AlAnrin <almia.anrin@gmail.com>
2019-05-06 08:55:00 +0300

    fixed and add 10, 15, 20, 25, 30

--- src/App.js
+++ src/App.js
@@ -5,27 +5,35 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        let board = [];
+        let board = new Array(39).fill(null);
 
         this.changeBoard(20, board);
 
         this.state = {
             board: board,
-            row: [0, 1, 2, 3, 4],
-            col: [1, 2, 3, 4, 5, 6, 7, 8],
+            row: 4,
+            col: 8,
             now: -1,
             select: 20,
-            canSelect: [20, 30]
+            canSelect: [10, 15, 20, 25, 30],
+            win: false
         }
     }
 
-    recurse = (board, min, max) => {
-        const now = this.randomInteger(min, max);
-        if (board.filter(b => b === now).length < 2) {
-            board.push({num: now, isVisible: false, isOpen: false});
-            return;
+    changeBoard(max, board) {
+        for (let i = 1; i <= max; i++) {
+            this.recurse(board, i, 0, 2 * max - 1);
+            this.recurse(board, i, 0, 2 * max - 1);
         }
-        else this.recurse(board, min, max);
+        console.log(board);
+    }
+
+    recurse = (board, i, min, max) => {
+        const now = this.randomInteger(min, max);
+        if (board[now] == null) {
+            board[now] = {num: i, isVisible: false, isOpen: false};
+        }
+        else this.recurse(board, i, min, max);
     };
 
     randomInteger = (min, max) => {
@@ -40,6 +48,10 @@ class App extends Component {
         });
     }
 
+    winner() {
+        this.setState({win: true});
+    }
+
     handleProps(index) {
         if (!this.state.board[index].isOpen) {
             let newBoard = this.state.board;
@@ -48,7 +60,7 @@ class App extends Component {
             if (this.state.now === -1)
                 this.setState({now: index});
             else {
-                if (this.state.board[this.state.now].num === this.state.board[index].num) {
+                if (this.state.board[this.state.now].num === this.state.board[index].num && index !== this.state.now) {
                     let newBoard = this.state.board;
                     newBoard[index].isOpen = true;
                     newBoard[this.state.now].isOpen = true;
@@ -66,60 +78,86 @@ class App extends Component {
                 }
             }
         }
-    }
-
-    changeBoard(max, board) {
-        for (let i = 0; i < 2 * max; i++) {
-            this.recurse(board, 1, max);
-        }
+        if (this.state.board.filter(b => b.isOpen).length === this.state.board.length)
+            this.winner();
     }
 
     handleSelect(select) {
         let row, col;
         if (select === 30) {
-            row = [0, 1, 2, 3, 4, 5];
-            col = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            row = 5;
+            col = 10;
         }
         if (select === 20) {
-            row = [0, 1, 2, 3, 4];
-            col = [1, 2, 3, 4, 5, 6, 7, 8];
+            row = 4;
+            col = 8;
+        }
+        if (select === 10) {
+            row = 4;
+            col = 5;
+        }
+        if (select === 15) {
+            row = 5;
+            col = 6;
+        }
+        if (select === 25) {
+            row = 5;
+            col = 10;
         }
         let board = [];
         this.changeBoard(select, board);
         this.setState({board: board, select: select, row: row, col: col});
     }
 
+    playAgain = () => {
+        this.setState({win: false});
+        this.handleSelect(20);
+    };
+
     render() {
+        let div = [], row = [], selRow = 0;
+        for (let i = 0; i < this.state.board.length; i++) {
+            if ((i - selRow * this.state.col) < this.state.col) {
+                row.push(
+                    <button className="num_button card box" key={i}
+                            onClick={() => this.handleProps(i)}>
+                        {this.state.board[i].isVisible ?
+                            this.state.board[i].num : ' '}
+                    </button>
+                );
+            }
+            else {
+                selRow++;
+                div.push(<div className="row">{row}</div>);
+                row = [];
+                i--;
+            }
+        }
+        div.push(<div className="row">{row}</div>);
         return (
             <div className="App">
-                <header className="App-header">
-                    <div className="row">
-                        {
-                            this.state.canSelect.map(nums =>
-                                <button key={nums} onClick={() => this.handleSelect(nums)}>{nums}</button>
-                            )
-                        }
-                    </div>
-                    <div>
-                        {
-                            this.state.row.map(r =>
-                                <div className="row" key={r}>
-                                    {
-                                        this.state.col.map(c =>
-                                            <div className="box card" key={c}>
-                                                <button className="num_button card"
-                                                        onClick={() => this.handleProps(r * (this.state.row.length - 1) + c - 1)}>
-                                                    {this.state.board[r * (this.state.row.length - 1) + c - 1].isVisible ?
-                                                        this.state.board[r * (this.state.row.length - 1) + c - 1].num : ''}
-                                                </button>
-                                            </div>
-                                        )
-                                    }
-                                </div>
-                            )
-                        }
-                    </div>
-                </header>
+                {
+                    this.state.win ?
+                        <header className="App-header">
+                            <h1>YOU WIN</h1>
+                            <button className="niceBtn" onClick={this.playAgain}>play again?</button>
+                        </header>
+                        :
+                    <header className="App-header">
+                        <div className="row">
+                            {
+                                this.state.canSelect.map(nums =>
+                                    <button className="niceBtn" key={nums} onClick={() => this.handleSelect(nums)}>{nums}</button>
+                                )
+                            }
+                        </div>
+                        <div>
+                            {
+                                div
+                            }
+                        </div>
+                    </header>
+                }
             </div>
         )
     }
